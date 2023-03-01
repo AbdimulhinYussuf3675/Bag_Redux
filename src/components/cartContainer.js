8ec40c8c1ea26d5494051cedc67861f3e1e8584e
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-01 17:37:32 +0300

    Add calculate total tofixed method

--- src/components/cartContainer.js
+++ src/components/cartContainer.js
@@ -1,27 +1,27 @@
 import CartItem from "./cartItem";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
 
   if (amount < 1) {
     return (
       <section className="cart">
-        <hearder>
+        <header>
           <h2>Your collection</h2>
           <h4 className="empty-cart">Is currently emplty</h4>
-        </hearder>
+        </header>
       </section>
     );
   }
 
   return (
     <section className="cart">
-      <hearder>
+      <header>
         <h2>Your Collection</h2>
-      </hearder>
+      </header>
       <div>
         {cartItems.map((item) => {
           return <CartItem key={item.id} {...item} />;
@@ -29,13 +29,15 @@ const CartContainer = () => {
       </div>
       <footer>
         <hr />
-        <div className="cart-total">
-          <h3>total <span>${total}</span></h3>
+        <div className='cart-total'>
+          <h4>
+            total <span>${total.toFixed(2)}</span>
+          </h4>
         </div>
       </footer>
-      <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>
-          clear cart
-        </button>
+      <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+        clear cart
+      </button>
     </section>
   );
 };
