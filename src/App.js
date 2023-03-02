7ac6d6e75deecd1b6c27398eb4a749d99739b1e4
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-02 06:48:53 +0300

    Implement using API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/cartContainer";
 import { useSelector, useDispatch } from "react-redux";
-import { calculateTotal } from "./features/cart/cartSlice";
+import { calculateTotal, getCartItems } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 import Modal from "./components/modal";
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotal());
-  }, [cartItems]);
+  }, [cartItems, dispatch]);
+
+  useEffect(() => {
+    dispatch(getCartItems('random'))
+  }, [dispatch])
+
+  if(isLoading){
+    return <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  }
   return (
     <main>
       {isOpen && <Modal />}      
